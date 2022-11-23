ed58aa9306c845dcb835be3061af278321da1cd7
Dimas Neto <dimas_fcn@outlook.com>
2022-11-23 16:23:04 -0300

    products

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name('product');
+
+Route::get('/admin/products', [AdminProductController::class, 'index'])->name('admin.products');
+Route::get('/admin/products/edit', [AdminProductController::class, 'edit'])->name('admin.products.edit');
