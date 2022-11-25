0cafa82e4932cfb2385267035f30b0081031fece
Dimas Neto <dimas_fcn@outlook.com>
2022-11-25 19:00:18 -0300

    finish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,26 @@
 
 @section('content')
 
+<section>
+    <form
+    method="get"
+    action="/"
+    class="flex justify-center items-center space-x-5"
+    >
+        <div>
+            <input type="text" id="search" name="search"
+                value="{{ request()->search }}"
+                class="w-[330px] bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+        </div>
+        <div class="flex items-center space-x-2">
+            <button type="submit" class="flex text-white bg-indigo-500 border-0 py-2 px-6 -ml-2 focus:outline-none hover:bg-indigo-600 rounded">Pesquisar</button>
+            <a href="/">Limpar</a>
+        </div>
+    </form>
+</section>
+
 <section class="text-gray-600">
-    <div class="container px-5 py-24 mx-auto">
+    <div class="container px-5 py-16 mx-auto">
         <div class="flex flex-wrap -m-4">
             @foreach($products as $product)
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
