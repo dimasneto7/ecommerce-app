a7fb1c95eaad9740fb77f0864521c9f5922a6038
Dimas Neto <dimas_fcn@outlook.com>
2022-11-24 21:34:11 -0300

    edit

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -2,17 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 
 class AdminProductController extends Controller
 {
     public function index()
     {
-        return view('admin.products');
+        $products = Product::all();
+
+        return view('admin.products', compact('products'));
     }
 
-    public function edit()
+    // Mostrar a página de editar
+    public function edit(Product $product)
     {
-        return view('admin.product_edit');
+
+        return view('admin.product_edit', [
+            'product' => $product
+        ]);
+    }
+
+    // Recebe requisição para update
+    public function update(Request $request, Product $product)
+    {
+        $input = $request->validate([
+            'name' => 'string|required',
+            'price' => 'string|required',
+            'stock' => 'integer|nullable',
+            'cover' => 'file|nullable',
+            'description' => 'string|nullable',
+        ]);
+
+        if (!empty($input['cover']) && $input['cover']->isValid()) {
+            $file = $input['cover'];
+            $path = $file->store('products');
+            $input['cover'] = $path;
+        }
+
+        $product->fill($input);
+        $product->save();
+        return Redirect::route('admin.products');
+
+    }
+
+    // mostrar página de criar
+    public function create()
+    {
+        return view('admin.product_create');
+    }
+
+    // Receber a requisição de criar
+    public function store(Request $request)
+    {
+        $input = $request->validate([
+            'name' => 'string|required',
+            'price' => 'string|required',
+            'stock' => 'integer|nullable',
+            'cover' => 'file|nullable',
+            'description' => 'string|nullable',
+        ]);
+
+        $input['slug'] = Str::slug($input['name']);
+
+        if (!empty($input['cover']) && $input['cover']->isValid()) {
+            $file = $input['cover'];
+            $path = $file->store('products');
+            $input['cover'] = $path;
+        }
+
+        Product::create($input);
+
+        return Redirect::route('admin.products');
     }
 }
--- resources/views/admin/product_create.blade.php
+++ resources/views/admin/product_create.blade.php
@@ -5,34 +5,42 @@
     <div class="container px-5 py-24 mx-auto">
         <div class="lg:w-2/4 w-full mx-auto overflow-auto">
             <div class="flex items-center justify-between mb-2">
-                <h1 class="text-2xl font-medium title-font mb-2 text-gray-900">Editar produto</h1>
+                <h1 class="text-2xl font-medium title-font mb-2 text-gray-900">Adicionar produto</h1>
             </div>
 
-            <form enctype="multipart/form-data" action="{{ route('admin.product.update', $product->id) }}" method="POST">
+            <form enctype="multipart/form-data" method="POST" action="{{ route('admin.product.store') }}">
                 @csrf
-                @method('put')
                 <div class="flex flex-wrap">
                     <div class="p-2 w-1/2">
                         <div class="relative">
                             <label for="name" class="leading-7 text-sm text-gray-600">Nome do produto</label>
-                            <input value="{{ $product->name }}" type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <input type="text" id="name" name="name" value="{{ old('name') }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
+                        @error('name')
+                            <div class="text-red-400 text-sm">{{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="p-2 w-1/2">
                         <div class="relative">
                             <label for="name" class="leading-7 text-sm text-gray-600">Preço</label>
-                            <input  value="{{ $product->price }}"  type="text" id="price" name="price"
+                            <input type="text" id="price" name="price" value="{{ old('price') }}"
                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
+                        @error('price')
+                            <div class="text-red-400 text-sm">{{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="p-2 w-1/2">
                         <div class="relative">
                             <label for="name" class="leading-7 text-sm text-gray-600">Estoque</label>
-                            <input  value="{{ $product->stock }}"  type="text" id="stock" name="stock"
-                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <input type="text" id="stock" name="stock" value="{{ old('stock') }}"
+                                   class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
+                        @error('stock')
+                            <div class="text-red-400 text-sm">{{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="p-2 w-1/2">
@@ -41,6 +49,9 @@
                             <input type="file" id="cover" name="cover"
                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
+                        @error('cover')
+                            <div class="text-red-400 text-sm">{{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="p-2 w-full">
@@ -48,12 +59,15 @@
                             <label for="name" class="leading-7 text-sm text-gray-600">Descrição</label>
                             <textarea
                                 id="description" name="description"
-                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $product->description }}</textarea>
+                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ old('description') }}</textarea>
                         </div>
+                        @error('description')
+                        <div class="text-red-400 text-sm">{{ $message }}</div>
+                    @enderror
                     </div>
 
                     <div class="p-2 w-full">
-                        <button type="submit" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Editar</button>
+                        <button type="submit" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Adicionar</button>
                     </div>
 
                 </div>
